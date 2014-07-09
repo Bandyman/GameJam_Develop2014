1a0c180c71524857ea7797e7b65a430f9630b109
Lionel Mille <lmille@ad-dispatch.com>
2014-07-09 14:49:28 +0100

    Initial Commit

---
+++ Unity/Assets/_Scripts/GJ_MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GJ_MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+		public void BTNPressed_NewGame() {
+				Application.LoadLevel( "Game" ) ;
+		}
+}
